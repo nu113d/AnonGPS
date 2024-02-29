e0afaafd290e42aa4672304df60e553180f64062
nu113d <stoikevang@protonmail.com>
2024-02-29 22:27:06 +0200

    Added links to github

--- app/src/main/java/com/application/anongps/MainActivity.java
+++ app/src/main/java/com/application/anongps/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 //TrackActivity: Track my device
 //SearchActivity: Search for a device
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button trackBtn = (Button) findViewById(R.id.buttonTrack);
         Button searchBtn = (Button) findViewById(R.id.buttonSearch);
+
+        TextView howToTxt= (TextView) findViewById(R.id.HowtoText);
+        howToTxt.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView contributeTxt= (TextView) findViewById(R.id.ContributeText);
+        contributeTxt.setMovementMethod(LinkMovementMethod.getInstance());
 
         if (isLocationServiceRunning()){
             startTrackActivity(true);
